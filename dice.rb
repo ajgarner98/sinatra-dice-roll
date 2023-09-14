cd68ebab97a91bee9ec6c470fb7198457c9f6daf
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-14 19:03:55 +0000

    made the erb file and views folder

--- dice.rb
+++ dice.rb
@@ -16,18 +16,10 @@ end
 get("/giraffe")do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
-get("/")do
-"
-<h1>Dice Roll</h1>
-<ul>
-  <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-  <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-  <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-  <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-</ul>
-"
-end
 
+get("/")do
+  erb(:elephant)
+end
 
 get("/dice/2/6")do
   first_die = rand(1..6)
