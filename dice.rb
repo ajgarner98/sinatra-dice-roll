0109291c80fafe3926cdc6e181f224ae7a0de230
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-14 03:04:33 +0000

    installed better errors

--- dice.rb
+++ dice.rb
@@ -1,5 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_callers"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 get("/helloworld")do
   "Hello World"
 end
