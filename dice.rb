d3e4e5a1f81a5ff2461441fcbd0244b56d837d79
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-15 01:30:06 +0000

    made dice templates

--- dice.rb
+++ dice.rb
@@ -25,10 +25,11 @@ get("/dice/2/6")do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
+
+  
 end
 
 get("/dice/2/10")do
